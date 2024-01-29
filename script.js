2ba87ccdfcf42b0ec37ee5a95806a201d5e2e4e1
Igor Angelotti <105242564+IgorAngelotti@users.noreply.github.com>
2024-01-29 18:41:53 -0300

    mostrar artistas

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 const searchInput = document.getElementById('search-input');
-const resultArtists = document.getElementById('result-artists');
-const resultPlaylist = document.getElementById('result-playlists')
+const resultArtists = document.getElementById('result-artist');
+const resultPlaylists = document.getElementById('result-playlists');
 
-document.addEventListener('input', function() {
-    const searchTerm = searchInput.ariaValueMax.toLocaleLowerCase()
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result));
+}
+
+function displayResults(result) {
+    esconderPlaylist()
+    const artistsName = document.getElementById('artist-name');
+    const artistsImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistsName.innerText = element.name;
+        artistsImage.src = element.urlImg;
+    });
+    resultArtists.classList.remove('hidden');
+}
+
+function esconderPlaylist() {
+    resultPlaylists.classList.add('hidden');
+}
+
+searchInput.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
     if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden')
+        resultPlaylists.classList.remove('hidden');
+        resultArtists.classList.add('hidden');
+        return
     }
 
-})
+    requestApi(searchTerm);
+});
+
+
